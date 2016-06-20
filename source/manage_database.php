42f34c91752f9efec55ff1829d68a7aab7f8eea8
mattmoehr <mattmoehr@gmail.com>
2016-06-20 11:40:07 -0500

    schema manager is confusing

--- source/manage_database.php
+++ source/manage_database.php
@@ -8,18 +8,21 @@ include 'html_head.php';
 
 
 // make a list of the tables that go into shots
+// TODO move this into the settings table
 $shots_tables = array('settings_global',
-                      //'settings_shots',
-                      //'relationships',
+                      'settings_shots',
+                      'relationships',
                       'people',
                       'grants',
-                      //'events'
+                      'events'
                       );
 
 // check if tables exists / how many rows they have
 
 // user input for re/creating new tables, downloading data, etc.
 
+// remember that a schema *manager* is not the schema class itselt
+// schema managers have a whole different set of methods/properties
 $shots_schema = $db->getSchemaManager();
 
 $shots_database_exists = sqliteDatabaseExists($shots_schema);
@@ -48,23 +51,57 @@ if ( !$shots_database_exists ) {
 } else {
   // present the options for managing tables
 
-  // foreach ($shots_tables as $table) {
-  //   echo $table . '</br>';
+  foreach ($shots_tables as $table_name) {
+    echo '<div class="info-table">' . $table_name;
 
-  //   $table_exists = $shots_schema->tablesExist($table);
+    $table_exists = $shots_schema->tablesExist($table_name);
 
-  //   var_dump($table_exists);
+    // var_dump($table_exists);
 
-    // if ( $table_exists ){
-    //   foreach ($table->getColumns() as $column) {
-    //     echo '<div>';
-    //     print_r($column);
-    //     echo '</div>';
-    //   }
-    // } else {
-    //   echo 'does not exist. Create it?';
-    // }
-  // }
+    if ( $table_exists ){
+
+      $table_info = $shots_schema->listTableDetails($table_name);
+
+      echo '
+        <div class="info-table-detials">
+          <pre>
+            ' . print_r($table_info) .'
+          </pre>
+        </div>
+      ';
+
+      /*
+      // Column info
+      $columns = $shots_schema->listTableColumns($table);
+      echo '
+        <div class="info-table-column-set">
+          # of Columns: '. count($columns)
+      ;
+      foreach ($columns as $column) {
+        echo '
+          <div class="info-table-column">' . 
+            $column->getName() . ' ('. $column->getType() .') 
+          </div>
+        ';
+      }
+      echo '</div>'; // close info-table-column-set
+
+      // Key info
+      $keys["indexes"] = 
+      */
+
+    } else {
+      echo '
+        <div class="info-table-dne">
+          Does not exist. 
+          <button type="button" class="btn btn-default" data-ajax="true" data-ajax-target="page" data-ajax-page="/lib/shots/setup/database/create_entity_grant.php">
+            Create it?
+          </button>
+        </div>
+      ';
+    }
+    echo '</div>'; // close info-table
+  }
 
 } // end if shots database exists
 
