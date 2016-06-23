e14e528cbd70b22e5d26555bb2c914d2a6924a9c
mattmoehr <mattmoehr@gmail.com>
2016-06-23 14:35:18 -0500

    clear out foo

--- source/foo.php
+++ source/foo.php
@@ -4,52 +4,19 @@ include './lib/all_pages.php';
 include 'html_doctype.php';
 include 'html_head.php';
 
-$new_include_path = get_include_path();
-
-$this_request["page"] = 'create_settings_global';
-include('setup/database/'. $this_request['page'] . '.php');
 ?>
+
+
 
 <body>
 
-<script type="text/javascript">
-  var req = {};
-  req.target   = 'entity';
-  req.action   = 'updateGrant';
-  req.table    = 'grants';
-  req.params   = [];
 
-  // get record id
-  // this requires the html to be marked up in a specific way
-  // req.params.push($(this).closest('.record').find('#grant_id').val());
 
-  // field name and new value;
-  // req.params.push($(this).attr('id'));
-  // req.params.push($(this).val());
 
-  console.log(req);
 
-  $.post('/lib/ajax_handler.php', { "request": req })
-         .done( function(d){
-          console.log(d);
-
-         })
-         .fail( 
-           function(d){
-             console.log('ajax post failed');
-             console.log(d);
-           }
-         )
-         .always()
-         ;
-</script>
+<?php include 'html_footer.php'; ?>
 
 </body>
-
-<?php
-
-include './lib/html_footer.php';
-
-?>
+</html>
 
 
