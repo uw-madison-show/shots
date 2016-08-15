49020a7ac847be5e015c376c0e22edf95b234cb1
mattmoehr <mattmoehr@gmail.com>
2016-08-15 10:55:30 -0500

    fix table views to use new initializeTable function

--- source/views/table_all_grants.php
+++ source/views/table_all_grants.php
@@ -18,7 +18,7 @@ include 'shots/entities/grants.php';
 <div class="container-fluid">
   <div class="row">
     <div class="col-xs-12 page-content">
-      <div id="table-holder" data-entity-name="grants">
+      <div id="table-holder" data-target="entity" data-entity-name="grants">
         <button id="button-add-row" type="button" class="btn btn-default" data-ajax="false">+ Add Row</button>
         <!-- TODO add a "Delete this Row" button. Maybe use context menu? -->
       </div>
@@ -29,35 +29,6 @@ include 'shots/entities/grants.php';
     </div>
   </div>
 </div>
-
-<?php
-// $table_name = 'grants';
-// $edits = array('title' => 'my new title',
-//                'status' => 'done'
-//                );
-// $key_field = 'grant_id';
-// $id = '12';
-
-// $key_field = $db->quoteIdentifier($key_field);
-
-// $results = array();
-
-// foreach ($edits as $field => $value) {
-//     $f = $db->quoteIdentifier($field);
-//     $qb = $db->createQueryBuilder();
-//     $qb
-//       ->select($field)
-//       ->from($table_name)
-//       ->where($key_field . ' = ?' )
-//       // ->setParameter(0, $key_field)
-//       ->setParameter(0, $id)
-//       ;
-//     $stmt = $qb->execute();
-//     $res = $stmt->fetchColumn(0);
-//     $results[] = $res;
-// }
-
-?>
 
 <?php include 'html_footer.php'; ?>
 
--- source/views/table_all_people.php
+++ source/views/table_all_people.php
@@ -11,26 +11,29 @@ include_once 'shots/entities/people.php';
 
 <body>
 
+<?php include 'html_navbar.php'; ?>
 
 <!-- This div will eventually hold the table.
      The Add Row button must go inside the table-holder div.
-     You can use as many data- attributes as you want for meta-data or parameters or whathave you.
-     The data-entity-name attribute is required, so js functions know which table to manipulate.
+     You can use as many data- attributes as you want for meta-data or 
+     parameters or whathave you.
+
+     The data-entity-name and data-target attributes are required, so 
+     js functions know which table to manipulate.
 -->
 
-<div id="table-holder" data-entity-name="people" data-key-field="email">
-  <button id="button-add-row" type="button" class="btn btn-default" data-ajax="false">+ Add Row</button>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-xs-12 page-content">
+      <div id="table-holder" data-target="entity" data-entity-name="people">
+        <button id="button-add-row" type="button" class="btn btn-default" data-ajax="false">+ Add Row</button>
+      </div>
+
+      <div id="autosave-message-holder"></div>
+      <div id="autosave-error-message-holder"></div>
+    </div>
+  </div>
 </div>
-
-
-
-<!-- TODO add a "Delete this Row" button. Maybe use context menu? -->
-
-
-
-<div id="autosave-message-holder"></div>
-
-<div id="autosave-error-message-holder"></div>
 
 <?php include 'html_footer.php'; ?>
 
@@ -44,7 +47,7 @@ include_once 'shots/entities/people.php';
 
     /**********************************************************/
 
-    initializeTable('people', 'person_id');
+    initializeTable('entity', 'people', 'person_id');
 
     /**********************************************************/
 
