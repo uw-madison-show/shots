71a78498f90d3548d0fea06a46acecfb3870a9ee
mattmoehr <mattmoehr@gmail.com>
2016-08-18 14:34:28 -0500

    update the deploy script (cpr, remove, new file list)

--- tools/copy_to_deploy.js
+++ tools/copy_to_deploy.js
@@ -6,23 +6,61 @@ var fs = require('fs');
 
 // going to use the path module to make this Win/*nix transferable
 var path = require('path');
+var copy = require('cpr').cpr;
+var rm   = require('remove');
 
-var ncp = require('ncp').ncp;
+// set up cpr
+var copy_options = {};
+copy_options.filter = filenameFilter;
+copy_options.overwrite = true;
 
-ncp.limit = 8;
+// function for ignoring files; defined globally across the whole project
+// cpr documentation: "f you give it a RegExp, it will use that to test the filename as they are being gathered. If it passes, it will be removed. If you give it a function, it will use that with Array.filter on the list of files.""
+function filenameFilter (filename){
+  return true;
+}
 
+
+// TODO make the root folder a CLI parameter
 // root folder for the project
 var root_folder = path.normalize('/Users/Moehr/Documents/GitHub/shots/');
 // input/output folders
 var destination_folder = path.join(root_folder, 'deploy');
 var source_folder      = path.join(root_folder, 'source');
 
-// make an array of the folders in `source` that will be copied
-var source_folders = [];
 
-source_folders.push( 'includes' );
-source_folders.push( 'lib' );
-source_folders.push( 'setup' );
+// clean out the deploy folder
+try {
+  rm.removeSync(destination_folder);
+  console.log('removed ' + destination_folder);
+} catch (err) {
+  console.log(err);
+}
+
+
+
+try {
+  // make an empty deploy folder
+  fs.mkdirSync(destination_folder);
+  // make an empty database & file folders
+  var empty_folders = [];
+  empty_folders.push(path.join(destination_folder, 'database'));
+  empty_folders.push(path.join(destination_folder, 'database/files'));
+  for (var i = 0; i < empty_folders.length; i++) {
+    var this_empty = empty_folders[i];
+    // console.log(this_empty);
+    fs.mkdirSync(this_empty);
+  }
+  console.log('created empty folders');
+} catch (err) {
+  console.log(err);
+}
+
+// make an array of the folders in `source` that will be copied
+var source_folders = ['includes',
+                      'lib',
+                      'views',
+                      ];
 
 
 // do the copying of the folders
@@ -30,15 +68,18 @@ for (var i = 0; i < source_folders.length; i++) {
   var this_folder = source_folders[i];
   var src = path.join(source_folder, this_folder);
   var des = path.join(destination_folder, this_folder);
-  ncp(src, des, function(err){
+  copy(src, des, function(err){
     if (err) { return console.error(err); }
   });
 }
+console.log('done cloning folders');
+
 
 // copy the top level files
-source_files = [];
-source_files.push('grant.php');
-source_files.push('index.html');
+source_files = ['delete.php',
+                'index.php',
+                'manage_database.php'
+                ];
 
 for (var i = 0; i < source_files.length; i++) {
   var this_file = source_files[i];
@@ -50,19 +91,10 @@ for (var i = 0; i < source_files.length; i++) {
     return console.log(err);
   }
 }
+console.log('done copying files');
 
 
 
-// make empty folders as necessary
-var empty_folders = [];
-empty_folders.push(path.join(destination_folder, 'database'));
-
-for (var i = 0; i < empty_folders; i++) {
-  var this_empty = empty_folders[i];
-  fs.mkdir(this_empty, function(err){
-    if (err) { return console.log(err); }
-  });
-}
 
 
 
