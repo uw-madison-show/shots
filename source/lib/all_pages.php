8c807e895a081da1f6fbb14b09b38d2d5215efe4
mattmoehr <mattmoehr@gmail.com>
2016-08-25 16:54:10 -0500

    make app_root dynamic
    Noticed when I moved from local dev machine to testing on webhost that
    all my links and includes were breaking. This is probably not a complete
    fix, but the test site is minimally functional.

--- source/lib/all_pages.php
+++ source/lib/all_pages.php
@@ -1,21 +1,42 @@
 <?php
 
-// handle includes paths on multiple dev/test/prod environments
-$old_include_path = get_include_path();
-$doc_root = $_SERVER['DOCUMENT_ROOT'];
 
-$app_include_folder = $doc_root . '/includes';
+// handle includes paths on multiple dev/test/prod environments
+// this assumes that all_pages.php is in this location:
+// <webroot>/<approot>/lib/all_pages.php
+// we will extract the <approot> portion:
+$doc_root = $_SERVER['DOCUMENT_ROOT'];
+$all_pages_directory = dirname(__FILE__);
+
+if ( ($doc_root . DIRECTORY_SEPARATOR . 'lib') === $all_pages_directory ) {
+  // this means that shots is running from the <webroot>, we can assume that there is no <approot>
+  $app_root = '';
+} else {
+  $app_root_pattern = "/[\/\\\\]([\\.a-z0-9_-]+)[\/\\\\]lib/i";
+  $matches = array();
+  preg_match($app_root_pattern, $all_pages_directory, $matches);
+  if ( !empty($matches[1]) ){
+    // since we found a 
+    $app_root = '/' . $matches[1];
+  } else {
+    $app_root = '';
+  }
+}
+
+
+$old_include_path = get_include_path();
+$app_include_folder = $doc_root . $app_root . '/includes';
 $for_preg = '/' . preg_quote($app_include_folder, '/') . '/';
 if ( preg_match($for_preg, $old_include_path) !== 1 ){
-  set_include_path($old_include_path . ';' . $app_include_folder);
+  set_include_path($old_include_path . PATH_SEPARATOR . $app_include_folder);
 }
 // $new_include_path = get_include_path();
 
 $old_include_path = get_include_path();
-$app_lib_folder = $doc_root . '/lib';
+$app_lib_folder = $doc_root . $app_root . '/lib';
 $for_preg2 = '/' . preg_quote($app_lib_folder, '/') . '/';
 if ( preg_match($for_preg2, $old_include_path) !== 1 ){
-  set_include_path($old_include_path . ';' . $app_lib_folder);
+  set_include_path($old_include_path . PATH_SEPARATOR . $app_lib_folder);
 }
 
 // Auto class loader for Doctrine stuff
--- source/lib/html_head.php
+++ source/lib/html_head.php
@@ -1,11 +1,22 @@
 <?php
 
+require_once 'all_pages.php';
+
 // TODO add local fallbacks for all the cdn libs
 
 echo '
-  <head>
+    <head>
     <title>Output Tracking</title>
-  ';  
+    ';  
+
+// these are php server vars that need to be set as 'global' type javascript vars
+echo '
+    <script>
+        var app_root = "' . $app_root .'";
+        var doc_root = "' . $doc_root .'";
+    </script>
+    ';
+
 
 // JQuery from Google cdn
 echo '
@@ -26,35 +37,35 @@ echo '
 // Handsontable from local
 // see: https://github.com/handsontable/handsontable
 echo '
-    <script src="/includes/handsontable/handsontable.full.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="/includes/handsontable/handsontable.full.min.css">
+    <script src="'. $app_root . '/includes/handsontable/handsontable.full.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="'. $app_root .'/includes/handsontable/handsontable.full.min.css">
     ';
 
 // moment js for handling date and time stuff
 echo '
-    <script src="/includes/moment/moment-2-14-1.min.js"></script>
+    <script src="'. $app_root .'/includes/moment/moment-2-14-1.min.js"></script>
     ';
 
 // fullcalendar JS and CSS
 // from fullcalendar docs: "jQuery and Moment must be loaded before FullCalendar's JavaScript."
 echo '
-    <link rel="stylesheet" href="/includes/fullcalendar/fullcalendar.css" />
-    <script src="/includes/fullcalendar/fullcalendar.js"></script>
+    <link rel="stylesheet" href="'. $app_root .'/includes/fullcalendar/fullcalendar.css" />
+    <script src="'. $app_root .'/includes/fullcalendar/fullcalendar.js"></script>
     ';
 
 // file upload
 // source: https://github.com/blueimp/jQuery-File-Upload
 echo '
-    <script src="/includes/jQuery-File-Upload/jquery.ui.widget.js"></script>
-    <script src="/includes/jQuery-File-Upload/jquery.iframe-transport.js"></script>
-    <script src="/includes/jQuery-File-Upload/jquery.fileupload.js"></script>
+    <script src="'. $app_root .'/includes/jQuery-File-Upload/jquery.ui.widget.js"></script>
+    <script src="'. $app_root .'/includes/jQuery-File-Upload/jquery.iframe-transport.js"></script>
+    <script src="'. $app_root .'/includes/jQuery-File-Upload/jquery.fileupload.js"></script>
      ';
 
 
 // custom JS and CSS for the shots database
 echo '
-    <script src="/lib/js/shots.js"></script>
-    <link rel="stylesheet" type="text/css" href="/lib/css/shots.css">
+    <script src="'. $app_root .'/lib/js/shots.js"></script>
+    <link rel="stylesheet" type="text/css" href="'. $app_root .'/lib/css/shots.css">
     ';
 
 echo '</head>';
