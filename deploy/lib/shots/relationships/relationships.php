fb7254ad1ff81f8d156483a81e20b504f741746a
mattmoehr <mattmoehr@gmail.com>
2016-09-07 14:35:22 -0500

    another deploy

--- deploy/lib/shots/relationships/relationships.php
+++ deploy/lib/shots/relationships/relationships.php
@@ -73,11 +73,24 @@ function relationshipsFetch( $entity_type = false, $entity_id = false, $format =
   return $return_array;
 }
 
+/**
+ * Add a row to the relationship table.
+ *
+ * Can be used to relate any two records in the database.
+ *
+ * @param string $from_entity_type e.g. 'people'
+ * @param string $from_entity_id The value of the primary key of the entity table specified in $from_entity_type.
+ * @param string $to_entity_type e.g. 'documents'
+ * @param string $to_entity_id The value of the primary key of the entity table speicifed in $to_entity_type
+ * @param string $relationship_type Defaults to 'is_related_to'. Currently not used in queries or reports, but different types of relationships could be used for customization.
+ *
+ * @return boolean TRUE when a relationship is added an there are no errors. FALSE when there is an error or if the relationship was not added, e.g. it already existed.
+ */
+
 function relationshipsAdd($from_entity_type = FALSE, $from_entity_id = FALSE, $to_entity_type = FALSE, $to_entity_id = FALSE, $relationship_type = 'is_related_to')
 {
-  $timer_start = microtime(true);
+  // $timer_start = microtime(true);
   global $db;
-
 
   if (!$from_entity_type or !$from_entity_id or !$to_entity_type or !$to_entity_id) {
     trigger_error('Missing parameters from relationshipsAdd().');
@@ -101,7 +114,7 @@ function relationshipsAdd($from_entity_type = FALSE, $from_entity_id = FALSE, $t
     trigger_error($to_entity_type . ' is not a valid entity.');
     return FALSE;
   }
-  $checks_done = microtime(true);
+  // $checks_done = microtime(true);
 
   // test if the id values exist in the entity tables;
   $from_record = getRecord($from_entity_type, $from_entity_id);
@@ -116,7 +129,7 @@ function relationshipsAdd($from_entity_type = FALSE, $from_entity_id = FALSE, $t
     trigger_error('Could not find ID '. $to_entity_id . ' in table ' . $to_entity_type);
     return FALSE;
   }
-  $getRecord_done = microtime(true);
+  // $getRecord_done = microtime(true);
 
   // check if this relationship already exists
 
@@ -134,12 +147,12 @@ function relationshipsAdd($from_entity_type = FALSE, $from_entity_id = FALSE, $t
                      'relationship_id',
                      $new_id
                      );
-  $updateRecord_done = microtime(true);
+  // $updateRecord_done = microtime(true);
 
-  echo '<div>checks '. ($checks_done - $timer_start) . '</div>';
-  echo '<div>getRecord '. ($getRecord_done - $checks_done) . '</div>';
-  echo '<div>addRecord '. ($addRecord_done - $getRecord_done). '</div>';
-  echo '<div>updateRecord '. ($updateRecord_done - $addRecord_done) . '</div>';
+  // echo '<div>checks '. ($checks_done - $timer_start) . '</div>';
+  // echo '<div>getRecord '. ($getRecord_done - $checks_done) . '</div>';
+  // echo '<div>addRecord '. ($addRecord_done - $getRecord_done). '</div>';
+  // echo '<div>updateRecord '. ($updateRecord_done - $addRecord_done) . '</div>';
 
   return $ck;
 }
---
+++ deploy/lib/widget_upload_documents.php
@@ -0,0 +1,96 @@
+<?php
+
+include_once 'shots/entities/documents.php';
+include_once 'shots/relationships/relationships.php';
+
+$related_files_html = '';
+
+if ( empty($this_table) or empty($this_id) ){
+  trigger_error('Document upload widget could not get data for either the table name or the entity id.');
+} else {
+
+  // get the related files
+  $related_files = array();
+  if ( !empty($related_entites) ){
+    if ( !empty($related_entites['documents']) ){
+      $related_files = $related_entites['documents'];
+    } 
+  } else {
+    $related_entites = relationshipsFetch($this_table, $this_id);
+    if ( !empty($related_entites['documents']) ){
+      $related_files = $related_entites['documents'];
+    }
+  }
+
+  // loop through related files and build a list of id numbers
+  $related_documents_id_array = array();
+  foreach ($related_files as $key => $this_file) {
+    $related_documents_id_array[] = $this_file['id'];
+  }
+
+  $related_documents = documentsFetch($related_documents_id_array);
+
+  // related documents could be empty if all the docs are set to inactive
+  if (!empty($related_documents)){
+    // build up the html for displaying download links
+    foreach ($related_documents as $key => $this_document) {
+      $related_files_html .= '<div class="related-documents">';
+      $related_files_html .= '<a href="'. $this_document['url'] .'">'. $this_document['name'] .'</a>';
+      $related_files_html .= '</div>';
+    }
+  }
+}
+
+
+
+
+  
+echo '
+    <div class="form-group">
+      <div class="col-xs-2">
+        <button id="open-file-upload-modal" type="button" class="btn btn-default">Attach file...</button>
+      </div>
+      <div class="col-xs-10">
+        '. $related_files_html .'
+      </div>
+    </div>
+    ';
+
+// TODO make the upload button work as a drag and drop target area
+// TODO move all of this code into javascript. the .fileupload() initializer is having too many glitches with the hidden div + clone into a modal approach that I started using
+echo '
+    <div id="file-upload-modal-message" style="display: none;">
+      <div class="form-horizontal">
+        <p>Upload file</p>
+
+        <div class="form-group">
+          <label class="control-label col-xs-4" for="title">Title</label>
+          <div class="col-xs-8">
+            <input id="title" name="title" class="form-control" type="text" placeholder="optional" />
+          </div>
+        </div>
+
+        <div class="form-group">
+          <label class="control-label col-xs-4" for="description">Description</label>
+          <div class="col-xs-8">
+            <input id="description" name="description" class="form-control" type="text" placeholder="optional" />
+          </div>
+        </div>
+
+        <input type="hidden" name="from_entity_type" id="from_entity_type" value="' . $this_table . '"/>
+        <input type="hidden" name="from_entity_id" id="from_entity_id" value="' . $this_id . '"/>
+
+        <div class="form-group">
+          <div class="col-xs-4 col-xs-offset-4">
+            <label for="file-upload-button" class="btn btn-default">Pick file<input type="file" id="file-upload-button" name="files[]" data-url="lib/file_handler.php" style="display: none;" />
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+    ';
+
+  
+  
+?>
+
