99cd359e56c1b575db6bed94907e03da547cd624
mattmoehr <mattmoehr@gmail.com>
2016-09-12 13:38:35 -0500

    add $app_root to URLs to make them server independent

--- source/lib/html_navbar.php
+++ source/lib/html_navbar.php
@@ -8,7 +8,7 @@ require_once 'all_pages.php';
 <nav class="navbar navbar-default navbar-fixed-top">
   <div class="container">
     <div class="navbar-header">
-      <a class="navbar-brand" href="/">SHOTS</a>
+      <a class="navbar-brand" href="<?php echo $app_root; ?>/">SHOTS</a>
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
@@ -18,17 +18,18 @@ require_once 'all_pages.php';
     </div>
     <div id="navbar" class="navbar-collapse collapse">
       <ul class="nav navbar-nav">
-        <li><a href="/<?php echo $app_root;?>">Home</a></li>
+        <li><a href="<?php echo $app_root;?>/">Home</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Jump To <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="/views/grants.php">Grants</a></li>
-            <li><a href="/views/people.php">People</a></li>
+            <li><a href="<?php echo $app_root; ?>/views/table_all_grants.php">Grants</a></li>
+            <li><a href="<?php echo $app_root; ?>/views/table_all_people.php">People</a></li>
+            <li><a href="<?php echo $app_root; ?>/views/manager_all_documents.php">Documents</a></li>
             <li><a href="#">Something else here</a></li>
             <li role="separator" class="divider"></li>
-            <li class="dropdown-header">Nav header</li>
-            <li><a href="#">Separated link</a></li>
-            <li><a href="#">One more separated link</a></li>
+            <li class="dropdown-header">Calendars</li>
+            <li><a href="<?php echo $app_root; ?>/views/calendar_month_all_events.php">Monthly</a></li>
+            <li><a href="#">Today</a></li>
           </ul>
         </li>
       </ul>
