a21f91ffd05832e3e45b1789432156bf98dd3119
mattmoehr <mattmoehr@gmail.com>
2016-09-12 13:39:25 -0500

    refactor the file manager UI

--- source/lib/shots/entities/documents.php
+++ source/lib/shots/entities/documents.php
@@ -12,6 +12,70 @@ $documents_primary_key = $sm->listTableIndexes('documents')['primary']->getColum
 
 $only_show_active_documents_default = TRUE;
 
+/**
+ * Format the bytes into human-readable file size.
+ *
+ * stolen from https://github.com/ael-code/dir-listing-bootstrap/blob/master/dir_listing_func.php (GNU GPL licensed)
+ *
+ * @param integer $size The number of bytes in the file.
+ *
+ * @return string File size with KB, MB, GB, etc. attached.
+ */
+function format_bytes($size)
+{
+      
+  $sizes = array('&nbsp&nbspB', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
+  $count = 0;
+  while( $count < (count( $sizes )-1) and $size > 1024){
+    $size = $size/1024;
+    $count ++;
+  }
+  $result = sprintf("%.2f %s", $size, $sizes[$count]);
+  return $result;
+}
+
+/**
+ * Pick an icon for an extension.
+ * 
+ * stolen from https://github.com/ael-code/dir-listing-bootstrap/blob/master/dir_listing_func.php (GNU GPL licensed)
+ *
+ * @param string $extension The file extension (defaults to 'txt').
+ *
+ * @return string The name of the glyphicon icon.
+ */
+function choose_icon( $extension = 'txt' )
+{
+  $ext = strtolower($extension); 
+  
+  $types = array(
+  "audio" => array("aif","iff","m3u","m4a","mid","mp3","mpa","ra","wav","wma"),
+  "video" => array("avi","mkv","3gp","asf","asx","3g2","flv","m4v","mov","mp4","mpg","rm","srt","swf","vob","wmv"),
+  "image" => array("gif","jpg","jpeg","png","psd","pspimage","tga","thm","tif","tiff","yuv","svg","bmp","dds"),
+  "text" => array("doc","docx","log","msg","odt","pages","rtf","tex","txt","wpd","wps","pdf"),
+  "zip" => array("7z","deb","gz","pkg","rar","rpm",".tar.gz","zip","zipx","jar"),
+  "disk" => array("bin","cue","dmg","iso","mdf","toast","vcd"),
+  "code" => array("java","c","class","pl","py","sh","cpp","cs","dtd","fla","h","lua","m","sln"),
+  "excel" => array("xlr","xls","xlsx")
+  );
+  $icons = array(
+  "audio" => "glyphicon-music",
+  "video" => "glyphicon-film",
+  "image" => "glyphicon-picture",
+  "text" => "glyphicon-file",
+  "zip" => "glyphicon-compressed",
+  "disk" => "glyphicon-record",
+  "code" => "glyphicon-indent-left",
+  "excel" => "glyphicon-list-alt",
+  "generic" => "glyphicon-unchecked"
+  );
+  foreach( $types as $elem_i => $elem_v){
+    if(in_array($ext, $elem_v)){
+      return $icons[$elem_i];
+    }
+  }
+  return $icons['generic'];
+}
+
 
 /**
  * Returns the HTML for a field in the documents table.
