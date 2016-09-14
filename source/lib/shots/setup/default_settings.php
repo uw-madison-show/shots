94bbf9f1c4cef523d55b2a26ab19dbf169cd0868
mattmoehr <mattmoehr@gmail.com>
2016-09-14 18:15:00 -0500

    make global settings just barely work

--- source/lib/shots/setup/default_settings.php
+++ source/lib/shots/setup/default_settings.php
@@ -1,5 +1,28 @@
 <?php
+// you should not edit this file
+// if you want to make custom settings, edit the file at <app_root>/lib/shots/intrnals/settings_global.php
+// we assume that the current file is located at <app_root>/lib/shots/setup/default_settings.php
+
+if (!isset($app_root)) {
+  require_once '../../all_pages.php';
+}
+
+// echo 'default_settings';
 
 // default settings will go here
+$entities = array(
+                  'grants' => array('table_creation' => $app_root . '/lib/shots/setup/database/create_entity_grants.php',
+                                    'php_functions' => '/lib/shots/entities/grants.php',
+                                    ),
+                  'people' => array('table_creation' => $app_root . '/lib/shots/setup/database/create_entity_people.php',
+                                    'php_functions' => '/lib/shots/entities/people.php',
+                                    ),
+                  'documents' => array('table_creation' => $app_root . '/lib/shots/setup/database/create_entity_documents.php',
+                                    'php_functions' => '/lib/shots/entities/documents.php',
+                                    ),
+                  'events' => array('table_creation' => $app_root . '/lib/shots/setup/database/create_entity_events.php',
+                                    'php_functions' => '/lib/shots/entities/events.php',
+                                    ),
+                  );
 
 ?>
