bf4116c46872fae59e86223b18be173f38af97a3
mattmoehr <mattmoehr@gmail.com>
2016-09-27 17:38:42 -0500

    improve the way shots guesses the server

--- deploy/lib/shots/internals/settings_global.php
+++ deploy/lib/shots/internals/settings_global.php
@@ -15,33 +15,33 @@ $authentication_services['google_signin_for_websites']['client_id'] = '146936374
 
 
 // change these arrays for your own dev/test/prod servers
-$servers['development'] = array('127.0.0.1', 'localhost');
+$servers['development'] = array('127.0.0.1:8080', 'localhost:3000');
 $servers['test']        = array('wwwtest.show.wisc.edu');
 $servers['production']  = array('show.wisc.edu');
 
 // this will pick out the server type
-$server_type = 'development';
-if ( in_array($_SERVER['SERVER_NAME'], $servers['development']) ){
+$server_type = '';
+if ( in_array($_SERVER['HTTP_HOST'], $servers['development']) ){
   $server_type = 'development';
-} else if ( in_array($_SERVER['SERVER_NAME'], $servers['test']) ){
+} else if ( in_array($_SERVER['HTTP_HOST'], $servers['test']) ){
   $server_type = 'test';
-} else if ( in_array($_SERVER['SERVER_NAME'], $servers['production']) ){
+} else if ( in_array($_SERVER['HTTP_HOST'], $servers['production']) ){
   $server_type = 'production';
 }
 
 
 // change up any of the settings you want to change based on which server this is running at
 // e.g. change your authentication credentials and upload directories
-if ( $server_type = 'development' ){
+if ( $server_type === 'development' ){
   $authentication_services['google_signin_for_websites']['client_id'] = '146936374460-leoa054enovpuksq875b9ignedeqnhsr.apps.googleusercontent.com';
 
-} else if ( $server_type = 'test' ){
+} else if ( $server_type === 'test' ){
   $authentication_services['google_signin_for_websites']['client_id'] = '146936374460-leoa054enovpuksq875b9ignedeqnhsr.apps.googleusercontent.com';
 
   $file_upload_options['upload_dir'] = '/var/www/vhosts/wwwtest.show.wisc.edu/httpdocs/shots/database/files/';
   $file_upload_options['upload_url'] = 'wwwtest.show.wisc.edu/database/files/';
 
-} else if ( $server_type = 'production' ){
+} else if ( $server_type === 'production' ){
 
   echo 'production server not yet configured in settings_global.php';
 
